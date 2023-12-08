5e146f0065ffd0912e7004b2165098230636f81e
simonblaue <sblaue@gmx.de>
2023-12-08 09:09:52 +0100

    not working

--- 2023/day_08/main.cpp
+++ 2023/day_08/main.cpp
@@ -7,9 +7,10 @@ By Simon Blaue
 #include <vector>
 #include <map>
 #include <tuple>
+#include <numeric>
 #include "../include/io.hpp"
 
-using std::vector, std::string, std::map, std::tuple;
+using std::vector, std::string, std::map, std::tuple, std::lcm;
 
 map<string,tuple<string,string>> getMaps(vector<string> data){
 
@@ -27,16 +28,13 @@ map<string,tuple<string,string>> getMaps(vector<string> data){
 	return nodes;
 }
 
-int partOne(vector<string> data){
+int partOne(vector<string> data, string actNode = "AAA"){
 
 	string instructinos = data[0];
 	map<string,tuple<string,string>> nodes = getMaps(data);
 	
-	string actNode = "AAA";
-	string end = "ZZZ";
-
 	int i = 0;
-	while (actNode != "ZZZ")
+	while (actNode[2] != 'Z')
 	{
 		for (char c: instructinos){
 			auto [nextLeftnode,nextRightNode] = nodes[actNode];
@@ -48,7 +46,7 @@ int partOne(vector<string> data){
 				actNode = nextRightNode;
 			}
 
-			if (actNode == "ZZZ"){
+			if (actNode[2] == 'Z'){
 				break;
 			}
 
@@ -74,49 +72,67 @@ int partTwo(vector<string> data){
 
 	//
 	vector<string> actNodes;
+	vector<int> v;
 	for (auto [key,val]: nodes){
 		if (key[2] == 'A'){
 			actNodes.push_back(key);
+			int n = partOne(data, key);
+			std::cout << n << " ";
+			v.push_back(n);
 		}
 	}
 
-	int i = 0;
-	while (!gotThere(actNodes))
-	{
-		for (char c: instructinos){
-			i++;
 
-			std::cout << "Going " << c <<" with  nodes: ";
-			for (string &actNode:actNodes){
-				std::cout << actNode << " ";
-				auto [nextLeftnode,nextRightNode] = nodes[actNode];
-				if (c == 'L')
-				{
-					actNode = nextLeftnode;
-				} else if (c == 'R'){
-					actNode = nextRightNode;
-				}
-			}
-			std::cout << "i="<< i << "\n";
-			if (gotThere(actNodes)){
-				return i;
-			}
-		}
-	}	
-	return i;
+	int res = std::accumulate(v.begin(), v.end(), 1, [](int a, int b){
+		return std::lcm(a,b);
+	});
+
+	while (v.size()>1){
+		int newVal	= std::lcm(*v.end()-1, *v.end());
+		v.pop_back();
+		v.pop_back();
+		v.push_back(newVal);
+	}
+
+	long long res2 = lcm(16271, lcm(22411, lcm(14429, lcm(18727, lcm (21797, 18113)))));
+
+	// int i = 0;
+	// while (!gotThere(actNodes))
+	// {
+	// 	for (char c: instructinos){
+	// 		i++;
+
+	// 		std::cout << "Going " << c << " with  nodes: ";
+	// 		for (string &actNode:actNodes){
+	// 			std::cout << actNode << " ";
+	// 			auto [nextLeftnode,nextRightNode] = nodes[actNode];
+	// 			if (c == 'L')
+	// 			{
+	// 				actNode = nextLeftnode;
+	// 			} else if (c == 'R'){
+	// 				actNode = nextRightNode;
+	// 			}
+	// 		}
+	// 		std::cout << "i="<< i << "\n";
+	// 		if (gotThere(actNodes)){
+	// 			return i;
+	// 		}
+	// 	}
+	// }	
+	return res2;
 }
 
 int main(void)
 {
 
 
-vector<string> data = readInput("testinput.txt");
+vector<string> data = readInput("input.txt");
 
 // int res1 = partOne(data);
-int res2 = partTwo(data);
+long long res2 = partTwo(data);
 
 //  std::cout << "The solution to part one is: " <<  res1 << std::endl;
-
+// 16271 22411 14429 18727 21797 18113
  std::cout << "The solution to part two is: " <<  res2 << std::endl;
 	return 0;
 }
